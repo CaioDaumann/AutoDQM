5067697634236f1d64278ae7f10806cbd8745170
root <root@cdaumann1234.cern.ch>
2024-04-11 09:21:47 +0200

    Added the histograms for DT_Online and made sure they are working. Still need to derive the Thresholds

--- plugins/beta_binomial.py
+++ plugins/beta_binomial.py
@@ -120,18 +120,18 @@ def beta_binomial(histpair, pull_cap=15, chi2_cut=10, pull_cut=10, min_entries=1
     is_outlier = data_hist_Entries >= min_entries and (chi2 > chi2_cut or abs(max_pull) > pull_cut)
 
     ## For subsystems with many many plots (e.g. DT_DOC1), only generate somewhat anomalous plots
-    sel_display_chi2    = -1.0
-    sel_display_maxPull = -1.0
-    if 'sel_display' in histpair.config.keys() and not histpair.config['sel_display'] is None:
-        for sel in histpair.config['sel_display']:
-            sels = sel.split('_')  ## Comparator, test, threshold
-            if not sels[0] == 'BB': continue
-            if sels[1] == 'Chi2':    sel_display_chi2    = float(sels[2])
-            if sels[1] == 'MaxPull': sel_display_maxPull = float(sels[2])
+    #sel_display_chi2    = -1.0
+    #sel_display_maxPull = -1.0
+    #if 'sel_display' in histpair.config.keys() and not histpair.config['sel_display'] is None:
+        #for sel in histpair.config['sel_display']:
+            #sels = sel.split('_')  ## Comparator, test, threshold
+            #if not sels[0] == 'BB': continue
+            #if sels[1] == 'Chi2':    sel_display_chi2    = float(sels[2])
+            #if sels[1] == 'MaxPull': sel_display_maxPull = float(sels[2])
     
         # Defining the Selective display thresholds based on the Chi2 and MaxPull choosen thresholds 
-        if chi2 < chi2_cut*0.5 and max_pull < pull_cut*0.5 and not is_outlier:
-            return None
+        #if chi2 < -100*chi2_cut*0.5 and max_pull < -100*pull_cut*0.5 and not is_outlier:
+        #    return None
 
     ## plotting
     # For 1D histograms, set pulls larger than pull_cap to pull_cap
